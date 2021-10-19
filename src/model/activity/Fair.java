4d7c3ce792fdd4ef972c46c271d87504072cbab8
inesfraccalvierii <ines.fraccalvieri@studio.unibo.it>
2021-10-19 18:01:41 +0200

    PMD warnings in activity

--- src/model/activity/Fair.java
+++ src/model/activity/Fair.java
@@ -1,84 +1,71 @@
 package model.activity;
 
 import java.util.ArrayList;
-
+import java.util.List;
 
 import model.person.ticket.PersonTicket;
 import view.model.activity.ActivityType;
 
 
 public class Fair implements Activity{
-	
 
-	private String name;
-	private int totPeople=0;
-	private int capacity;
-	private ActivityType activityType;
-	
-	
-	ArrayList<PersonTicket> personList= new ArrayList<PersonTicket>();
-	
-	
-	public Fair(String name, int capacity, ActivityType activityType) {
-		this.name = name;
-		this.capacity=capacity;
-		this.activityType = activityType;
-	}
-	
-	
-	public Fair(String name, int capacity, ActivityType activityType, int visitors) {
-		this.name = name;
-		this.capacity=capacity;
-		this.activityType = activityType;
-		this.totPeople=visitors;
-	}
-	
-	
-	public void addPerson(PersonTicket person) {
-		personList.add(person);
-		totPeople++;
-	}
+    private final String name;
+    private static int totPeople;
+    private final int capacity;
+    private final ActivityType activityType;
+    private final List<PersonTicket> personList = new ArrayList<>();
 
-	
-	public ArrayList<PersonTicket> getPeople() {
-		return personList;
-	}
-	
-	
-	public void removePerson() {
-		personList.clear();
-	}
-	
-	
-	public int getTotPeople() {
-		return totPeople;
-	}
-	
-	
-	public int getCapacity() {
-		return capacity;
-	}
+    public Fair(String name, int capacity, ActivityType activityType) {
+        this.name = name;
+        this.capacity=capacity;
+        this.activityType = activityType;
+    }	
 
-	public boolean controlAge(int age) {
-		if(age>12) {
-			return true;
-		}else {
-			return false;
-		}
-	}
+    public Fair(String name, int capacity, ActivityType activityType, int visitors) {
+        this.name = name;
+        this.capacity=capacity;
+        this.activityType = activityType;
+        this.totPeople=visitors;
+    }
 	
-	
-	public ActivityType getActivityType() {
-		return this.activityType;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public ArrayList<PersonTicket> getPeopleList() {
-		return personList;
-		
-	}
+    public void addPerson(PersonTicket person) {
+        personList.add(person);
+        totPeople++;
+    }
 
+    public List<PersonTicket> getPeople() {
+        return personList;
+    }	
+
+    public void removePerson() {
+        personList.clear();
+    }	
+
+    public int getTotPeople() {
+        return totPeople;
+    }	
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean controlAge(int age) {
+        if(age>12) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public ActivityType getActivityType() {
+        return this.activityType;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<PersonTicket> getPeopleList() {
+        return personList;	
+    }
 }
