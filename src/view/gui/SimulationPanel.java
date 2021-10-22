98f087ebbd0672b2aaf995a4c4a593c59b5e3d05
Enrica <enrica.contrini@studio.unibo.it>
2021-10-22 15:12:35 +0200

    add activitygui in SimulationPanel

--- src/view/gui/SimulationPanel.java
+++ src/view/gui/SimulationPanel.java
@@ -35,6 +35,11 @@ public class SimulationPanel extends JPanel {
     private Map<PersonTicket, Position<Integer, Integer>> map;
     private List<CircleImpl> adult = new ArrayList<>();
     private List<CircleImpl> baby = new ArrayList<>();
+    private List<Square> shop;
+    private List<Square> restaurant;
+    private List<Square> adultFair;
+    private List<Square> babyFair;
+
     private static final int ADULT_RADIUS = 20;
     private static final int BABY_RADIUS = 15;
     private final JFrame frame;
@@ -66,7 +71,7 @@ public class SimulationPanel extends JPanel {
         this.map.put(new PersonTicket(), new RandomPosition().randomPosition(map));
         panel.add(this, BorderLayout.CENTER);
         this.setBackground(Color.WHITE);
-        
+
         stop.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 SimulationPanel.this.frame.dispose();
@@ -78,56 +83,93 @@ public class SimulationPanel extends JPanel {
     }
 
     public void updateSimulation() {
-        final Map<PersonTicket, Position<Integer,Integer>> people = this.map;
+        final Map<PersonTicket, Position<Integer, Integer>> people = this.map;
         final List<CircleImpl> adult = new ArrayList<>();
         final List<CircleImpl> baby = new ArrayList<>();
-        for(PersonTicket p: people.keySet()) {
-            if(p.getAge()>12) {
-                adult.add(DesignPerson.createAdult(people.get(p).getFirst(), 
-                        people.get(p).getSecond(), ADULT_RADIUS));
-                System.out.println("Posizione di " + p.toString() + "= x: " + map.get(p).getFirst() + "y: " + map.get(p).getSecond() );
+        for (PersonTicket p : people.keySet()) {
+            if (p.getAge() > 12) {
+                adult.add(DesignPerson.createAdult(people.get(p).getFirst(), people.get(p).getSecond(), ADULT_RADIUS));
+                System.out.println("Posizione di " + p.toString() + "= x: " + map.get(p).getFirst() + "y: "
+                        + map.get(p).getSecond());
             } else {
-                baby.add(DesignPerson.createBaby(people.get(p).getFirst(), 
-                        people.get(p).getSecond(), BABY_RADIUS));  
+                baby.add(DesignPerson.createBaby(people.get(p).getFirst(), people.get(p).getSecond(), BABY_RADIUS));
             }
             this.adult = adult;
             this.baby = baby;
             this.repaint();
         }
-      }
+    }
+
     // draw oval
     public void paintComponent(Graphics g) {
         try {
             super.paintComponent(g);
-            if(this.adult.size() != 0) {
+            if (this.adult.size() != 0) {
                 for (CircleImpl adult : adult) {
                     g.setColor(adult.getColor());
                     g.fillOval(adult.getX(), adult.getY(), (int) adult.getRadius(), (int) adult.getRadius());
                 }
             }
-            if(this.baby.size() != 0) {
+            if (this.baby.size() != 0) {
                 for (CircleImpl baby : baby) {
                     g.setColor(baby.getColor());
                     g.fillOval(baby.getX(), baby.getY(), (int) baby.getRadius(), (int) baby.getRadius());
                 }
             }
+
+            for (Square babyFair : babyFair) {
+                g.setColor(babyFair.getColor());
+                g.fillRect(babyFair.getX(), babyFair.getY(), babyFair.getWidth(), babyFair.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawRect(babyFair.getX(), babyFair.getY(), babyFair.getWidth(), babyFair.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawString(babyFair.getName(), babyFair.getX(), babyFair.getY() + 30);
+            }
+
+            for (Square adultFair : adultFair) {
+                g.setColor(adultFair.getColor());
+                g.fillRect(adultFair.getX(), adultFair.getY(), adultFair.getWidth(), adultFair.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawRect(adultFair.getX(), adultFair.getY(), adultFair.getWidth(), adultFair.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawString(adultFair.getName(), adultFair.getX(), adultFair.getY() + 30);
+            }
+
+            for (Square restaurant : restaurant) {
+                g.setColor(restaurant.getColor());
+                g.fillRect(restaurant.getX(), restaurant.getY(), restaurant.getWidth(), restaurant.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawRect(restaurant.getX(), restaurant.getY(), restaurant.getWidth(), restaurant.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawString(restaurant.getName(), restaurant.getX(), restaurant.getY() + 30);
+            }
+
+            for (Square shop : shop) {
+                g.setColor(shop.getColor());
+                g.fillRect(shop.getX(), shop.getY(), shop.getWidth(), shop.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawRect(shop.getX(), shop.getY(), shop.getWidth(), shop.getHeight());
+                g.setColor(Color.BLACK);
+                g.drawString(shop.getName(), shop.getX(), shop.getY() + 30);
+            }
             this.adult.clear();
             this.baby.clear();
-        } catch(ConcurrentModificationException e) {
-                System.out.println(e.getMessage());
+        } catch (ConcurrentModificationException e) {
+            System.out.println(e.getMessage());
         }
+
     }
 
-    public Map<PersonTicket, Position<Integer, Integer>> getPeopleMap(){
+    public Map<PersonTicket, Position<Integer, Integer>> getPeopleMap() {
         System.out.println(this.map.size());
         return this.map;
     }
-    
+
     public void close() {
         this.frame.dispose();
         this.controller.stop();
     }
-          
+
 }
 
 
